c6596f1cd1750cff4d742fdd1eb2925b70f8a220
diogosalvadorb <diogosalvadorb@gmail.com>
2025-10-18 01:58:39 -0300

    teacher validators

---
+++ backend/VirtualClass.Application/Validators/TeacherValidators/UpdateTeacherCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using VirtualClass.Application.Commands.TeacherCommands.UpdateTeacher;
+
+namespace VirtualClass.Application.Validators.TeacherValidators
+{
+    internal class UpdateTeacherCommandValidator : AbstractValidator<UpdateTeacherCommand>
+    {
+        public UpdateTeacherCommandValidator()
+        {
+            RuleFor(x => x.TeacherId)
+                .NotEmpty().WithMessage("ID do professor é obrigatório.");
+
+            RuleFor(x => x.Bio)
+                .NotEmpty().WithMessage("Biografia é obrigatória.")
+                .MinimumLength(10).WithMessage("Biografia deve ter no mínimo 10 caracteres.")
+                .MaximumLength(1000).WithMessage("Biografia deve ter no máximo 1000 caracteres.");
+
+            RuleFor(x => x.Specialty)
+                .NotEmpty().WithMessage("Especialidade é obrigatória.")
+                .MinimumLength(3).WithMessage("Especialidade deve ter no mínimo 3 caracteres.")
+                .MaximumLength(200).WithMessage("Especialidade deve ter no máximo 200 caracteres.");
+        }
+    }
+}
