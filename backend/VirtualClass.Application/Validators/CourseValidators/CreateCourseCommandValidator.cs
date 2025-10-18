319c961ca196e685d89dd21aa161bc599677fdae
diogosalvadorb <diogosalvadorb@gmail.com>
2025-10-18 15:49:03 -0300

    course validators

---
+++ backend/VirtualClass.Application/Validators/CourseValidators/CreateCourseCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using VirtualClass.Application.Commands.CourseCommands.CreateCourse;
+
+namespace VirtualClass.Application.Validators.CourseValidators
+{
+    public class CreateCourseCommandValidator : AbstractValidator<CreateCourseCommand>
+    {
+        public CreateCourseCommandValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Título é obrigatório.")
+                .MinimumLength(3).WithMessage("Título deve ter no mínimo 3 caracteres.")
+                .MaximumLength(200).WithMessage("Título deve ter no máximo 200 caracteres.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Descrição é obrigatória.")
+                .MinimumLength(10).WithMessage("Descrição deve ter no mínimo 10 caracteres.")
+                .MaximumLength(2000).WithMessage("Descrição deve ter no máximo 2000 caracteres.");
+
+            RuleFor(x => x.Price)
+                .GreaterThanOrEqualTo(0).WithMessage("Preço não pode ser negativo.");
+
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("Categoria é obrigatória.")
+                .MaximumLength(100).WithMessage("Categoria deve ter no máximo 100 caracteres.");
+
+            RuleFor(x => x.TeacherId)
+                .NotEmpty().WithMessage("ID do professor é obrigatório.");
+        }
+    }
+}
